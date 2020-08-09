d2df29fe32fb189b48ebc46bd7d0e5c089fee9dd
Hannah Angle <hannahcangle@gmail.com>
2020-08-09 19:34:16 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,24 +1,16 @@
 def sorting(top)
   prime_numbers = (0..top).to_a
   prime_numbers[0] = prime_numbers[1] = nil
-
-  
   counter = 0
   prime_numbers.each do |i|
     next unless i
-
-    
-    break if i*i > top
+   break if i*i > top
     counter += 1
-    
     (i*i).step(top,i) { |y| prime_numbers[y] = nil }
   end
-
-  
-  puts "Solved for #{top} in #{counter} steps."
+puts "Solved for #{top} in #{counter} steps."
   prime_numbers.compact
 end
-
 def prime?(number)
   sorting(number).include?(number)
 end
