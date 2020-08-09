0c23dcdbc6cf37d48652e31c02101093ecfac1bf
Hannah Angle <hannahcangle@gmail.com>
2020-08-09 19:27:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,9 @@
 def prime?(x)
   if x == 1
     return true 
-  elsif x < 1
+  else x < 1
     return false
-  else
-  for y in 2..(x-1)
-    if (x % y) == 0
-      return false 
-    end
-  end 
+  end
 end
 
   true 
